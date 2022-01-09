ba8447fdb6ccb84561be99e3401283a8e28c03aa
schollsebastian <scholl.sebastian@gmx.at>
2022-01-09 18:02:23 +0100

    Update the routes to include sales

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,16 +1,18 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {HomeComponent} from "./home/home.component";
-import {TreeComponent} from "./tree/tree.component";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
+import { TreeComponent } from "./tree/tree.component";
+import { SaleComponent } from './sale/sale.component';
 
 const routes: Routes = [
-    {path: 'tree', component: TreeComponent},
-    {path: '**', component: HomeComponent, pathMatch: 'full'}
+  { path: 'tree', component: TreeComponent },
+  { path: 'sale', component: SaleComponent },
+  { path: '**', component: HomeComponent, pathMatch: 'full' }
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule {
 }
