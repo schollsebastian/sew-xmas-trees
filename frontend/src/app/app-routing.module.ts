4a2c57556e7af40c7d16c5fb614118d94e776ce5
schollsebastian <scholl.sebastian@gmx.at>
2022-01-09 19:12:16 +0100

    Add pay-pal route

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
 import { TreeComponent } from "./tree/tree.component";
 import { SaleComponent } from './sale/sale.component';
+import { PayPalComponent } from './pay-pal/pay-pal/pay-pal.component';
 
 const routes: Routes = [
   { path: 'tree', component: TreeComponent },
   { path: 'sale', component: SaleComponent },
+  { path: 'pay-pal', component: PayPalComponent },
   { path: '**', component: HomeComponent, pathMatch: 'full' }
 ];
 
