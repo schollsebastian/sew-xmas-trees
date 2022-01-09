62b6f1a9e310a4957d1b9e8d005cd8608cf1331c
schollsebastian <scholl.sebastian@gmx.at>
2022-01-09 17:47:33 +0100

    Create SaleDataService

---
+++ frontend/src/app/sale-data.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { ITree } from './shared/backend.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SaleDataService {
+
+  private treeToSell: ITree | null;
+
+  constructor() {
+    this.treeToSell = null;
+  }
+
+  public set tree(tree: ITree | null) {
+    this.treeToSell = tree;
+  }
+
+  public get tree(): ITree | null {
+    return this.treeToSell;
+  }
+
+}
