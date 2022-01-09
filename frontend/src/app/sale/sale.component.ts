05452c28d021b0979c262d64082736fe4e42297c
schollsebastian <scholl.sebastian@gmx.at>
2022-01-09 18:00:22 +0100

    Create SaleComponent

---
+++ frontend/src/app/sale/sale.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { BackendService, ITree } from '../shared/backend.service';
+import { SaleDataService } from '../sale-data.service';
+
+@Component({
+  selector: 'app-sale',
+  templateUrl: './sale.component.html',
+  styleUrls: ['./sale.component.scss']
+})
+export class SaleComponent {
+
+  public tree: ITree;
+
+  public constructor(private readonly dataService: SaleDataService,
+                     private readonly backendService: BackendService) {
+    this.tree = dataService.tree!;
+  }
+
+}
